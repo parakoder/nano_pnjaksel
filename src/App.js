4109c1119f76554df35d9cb9f680aa4acd38e5e9
twinedo <twinedo@github.com>
2021-04-09 08:31:15 +0700

    add assets, add axios, add router dom, add page homepage dan form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Homepage from './pages/homepage/Homepage';
+import Form from './pages/form/Form';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className='App'>
+            <Switch>
+                <Route path='/form'>
+                    <Form />
+                </Route>
+                <Route path='/'>
+                    <Homepage />
+                </Route>
+            </Switch>
         </div>
     );
 }
