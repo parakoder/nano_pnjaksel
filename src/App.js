872edb9572fccef11f36b05686454d2437462d59
twinedo <twinedo@github.com>
2021-04-16 07:48:44 +0700

    ilangin logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
 import Form from './pages/form/Form';
 
 function App() {
-	return (
-		<>
-			<Switch>
-				<Route path='/form'>
-					<Form />
-				</Route>
-				<Route path='/'>
-					<Homepage />
-				</Route>
-			</Switch>
-		</>
-	);
+    return (
+        <>
+            <Switch>
+                <Route path='/form'>
+                    <Form />
+                </Route>
+                <Route path='/'>
+                    <Homepage />
+                </Route>
+            </Switch>
+        </>
+    );
 }
 
 export default App;
