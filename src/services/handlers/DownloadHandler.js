16f5ec2a815865bd9d227833c3a6b04f38232507
Twin Edo <venganzdektor26@gmail.com>
2021-04-21 23:29:12 +0700

    generate email send data

--- src/services/handlers/DownloadHandler.js
+++ src/services/handlers/DownloadHandler.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 export const DownloadHandler = async (id) => {
-    try {
-        const response = await axios.get(
-            `${process.env.REACT_APP_ROOT_API_DOWNLOAD}/download`,
-            {
-                params: { id: id },
-                headers: {
-                    Accept: 'application/pdf',
-                },
-                responseType: 'arraybuffer',
-            }
-        );
-        console.log('res download', response);
-        return Promise.resolve(response.data);
-    } catch (error) {
-        console.log('err download', error.request);
-        return Promise.reject(error);
-    }
+	try {
+		const response = await axios.get(
+			`${process.env.REACT_APP_ROOT_API_DOWNLOAD}/download`,
+			{
+				params: { id: id },
+				headers: {
+					Accept: 'application/pdf',
+				},
+				responseType: 'arraybuffer',
+			}
+		);
+		console.log('res download', response);
+		return Promise.resolve(response);
+	} catch (error) {
+		console.log('err download', error.request);
+		return Promise.reject(error);
+	}
 };
--- src/services/handlers/GenerateHandler.js
+++ src/services/handlers/GenerateHandler.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
-export const GenerateEmail = async () => {
-    try {
-        const response = await axios.post(
-            `${process.env.REACT_APP_ROOT_API_DOWNLOAD}/generate`
-        );
-        console.log('res generate', response);
-        return Promise.resolve(response.data);
-    } catch (error) {
-        console.log('err generate', error.request);
-        return Promise.reject(error);
-    }
+export const GenerateEmail = async (data) => {
+	try {
+		const response = await axios.post(
+			`${process.env.REACT_APP_ROOT_API_DOWNLOAD}/generate`,
+			data
+		);
+		console.log('res generate', response);
+		return Promise.resolve(response.data);
+	} catch (error) {
+		console.log('err generate', error.request);
+		return Promise.reject(error);
+	}
 };
